22e42bd207f293e7e08aa364fbb4705db724ef38
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-01-31 17:44:14 +0100

    Create test_0015.java

---
+++ test_0015.java
@@ -0,0 +1,18 @@
+package org.yapix.math_sphere;
+
+public class Random {
+
+	public static void main(String[] args) {
+
+		Random randA = new Random();
+		double a1 = 6D * (-1);
+		System.out.println(a1 + " " + randA);
+		
+	}
+
+	public int nextInt(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
