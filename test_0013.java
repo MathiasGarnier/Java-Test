e2dbc93083d0eb650daf0f92bf3baa16056ddc1a
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-01-31 17:43:20 +0100

    Create test_0013.java

---
+++ test_0013.java
@@ -0,0 +1,14 @@
+package org.yapix.math_sphere;
+
+import java.time.LocalDateTime;
+
+public class Clock {
+
+	public static void main(String[] args) {
+
+		LocalDateTime d = LocalDateTime.now();
+		
+		System.out.println(d);
+	}
+
+}
