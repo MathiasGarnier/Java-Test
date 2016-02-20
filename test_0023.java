6436a9a92f366de5973be0789abf9db8625bb8e1
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-02-20 15:44:39 +0100

    Create test_0023.java

---
+++ test_0023.java
@@ -0,0 +1,21 @@
+package org.yapix.math_sphere;
+
+import java.util.Scanner;
+
+public class Log {
+
+	public static void main(String[] args) {
+		
+	int x = 15;	
+	Scanner sc = new Scanner(System.in);
+	String str = new String();
+	
+	str = sc.nextLine();
+	
+		while (str != "salut"){
+			for (int i = 0; i < x; i++){
+			str = sc.nextLine();
+			} 
+		}
+	}
+}
