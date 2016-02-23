0dcea9bf95b983d90748803c3b94ac4bc5e714b2
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-02-23 19:43:22 +0100

    Create test_0042.java

---
+++ test_0042.java
@@ -0,0 +1,15 @@
+package org.yapix.math_sphere;
+
+public class Hexadecimal {
+
+	public static void main(String[] args) {
+		
+		int n = 100;
+		
+		for (int i = 0; i < n; i++){
+			System.out.printf("%X\n", i);
+		}
+
+	}
+
+}
