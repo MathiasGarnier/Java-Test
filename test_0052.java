117ff178f44da6a1be02182f7e2259b40e56d3dd
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-03-03 19:49:47 +0100

    Create test_0052.java

---
+++ test_0052.java
@@ -0,0 +1,12 @@
+package fr.mathiasGarnier.testJeux;
+
+import fr.mathiasGarnier.testJeux.util.Plateau;
+
+public class Main {
+
+	public static void main(String[] args){
+	
+		System.out.println(Plateau.getPlateau(11, 11));
+		// result : [[I@2a139a55
+	}
+}
