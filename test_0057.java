027e1947f71f4f7f7e3f570bcf2d9717be5f29fa
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-24 20:19:06 +0200

    Create test_0057.java

---
+++ test_0057.java
@@ -0,0 +1,13 @@
+package fr.mathiasGarnier.reApprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+    b:
+        while(true){
+            int i = 0;
+            System.out.print(i);
+                if (i > 5) break b;
+        }
+    }
+}
