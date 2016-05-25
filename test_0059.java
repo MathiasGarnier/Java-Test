54c22ae72b251591eebe62f8402da4d9369b5f05
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-25 16:58:23 +0200

    Create test_0059.java

---
+++ test_0059.java
@@ -0,0 +1,12 @@
+package fr.mathiasGarnier.reApprentissage;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.print(d.getClass().toString());
+    }
+
+}
