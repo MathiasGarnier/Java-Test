74fd53c0cdfdd64ba7b42955fb5127450647f453
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-28 19:59:47 +0200

    Create ConvertisseurTemperature.java

---
+++ ConvertisseurTemperature.java
@@ -0,0 +1,48 @@
+package fr.mathiasGarnier.Apprentissage;
+
+
+/**
+ * Created by Yapix on 28/05/2016.
+ */
+public class ConvertisseurTemperatures {
+
+
+    public void convertirTempérature(int temperature, char convertirEn) {
+        switch (convertirEn) {
+            case 'F':
+                System.out.println("Celsius :");
+                int Celsius = (temperature - 32) * 5 / 9;
+
+                System.out.println(Celsius);
+                break;
+
+            case 'C':
+                System.out.println("Farenheit :");
+
+                int Farenheit = temperature * 9 / 5 + 32;
+
+                System.out.println(Farenheit);
+                break;
+
+            default:
+
+                break;
+        }
+    }
+}
+
+
+
+
+//Main class : 
+package fr.mathiasGarnier.Apprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConvertisseurTemperatures cT = new ConvertisseurTemperatures();
+        
+        cT.convertirTempérature(22, 'C');
+    }
+}
