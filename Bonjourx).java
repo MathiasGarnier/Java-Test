bbf9dfe71b03664edaabcb0c99a70fbd3c81e1c6
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-28 17:16:50 +0200

    Create Bonjourx).java

---
+++ Bonjourx).java
@@ -0,0 +1,20 @@
+package fr.mathiasGarnier.Apprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("########   #######  ##    ##       ##  #######  ##     ## ########");
+        System.out.println("##     ## ##     ## ###   ##       ## ##     ## ##     ## ##     ##");
+        System.out.println("##     ## ##     ## ####  ##       ## ##     ## ##     ## ##     ##");
+        System.out.println("########  ##     ## ## ## ##       ## ##     ## ##     ## ########");
+        System.out.println("##     ## ##     ## ##  #### ##    ## ##     ## ##     ## ##   ##");
+        System.out.println("##     ## ##     ## ##   ### ##    ## ##     ## ##     ## ##    ##");
+        System.out.println(" ########   #######  ##    ##  ######   #######   #######  ##     ##");
+        
+        /*
+          C'est beau hein x)
+        */
+        
+    }
+}
