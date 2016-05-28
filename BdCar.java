49d4c610e3b4339b214d017bdfe3201dc3c5b1c0
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-28 18:08:27 +0200

    Create BdCar.java

---
+++ BdCar.java
@@ -0,0 +1,11 @@
+package fr.mathiasGarnier.Apprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BdCar r = new BdCar();
+
+        System.out.println("VROUUUUUM VROUUUUUMMM x) : " + r.rouler(69));
+    }
+}
