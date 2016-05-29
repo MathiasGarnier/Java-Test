5d55d77199a5d4a10c038859e409d0989c934db9
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-05-29 15:27:26 +0200

    Create intGetProtectionDomain.java

---
+++ intGetProtectionDomain.java
@@ -0,0 +1,18 @@
+package fr.mathiasGarnier.Apprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print(int.class.getProtectionDomain());
+        /*
+          ProtectionDomain  null
+         null
+         <no principals>
+         java.security.Permissions@1540e19d (
+         ("java.security.AllPermission" "<all permissions>" "<all actions>")
+        )
+        */
+    }
+
+}
