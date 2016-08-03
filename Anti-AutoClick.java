c51dd0564d5cbc74389b0988729ffed6016d67ea
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-08-03 13:50:51 +0200

    Create Anti-AutoClick.java

---
+++ Anti-AutoClick.java
@@ -0,0 +1,58 @@
+package fr.yapix;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerAnimationType;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.broadcastMessage("Animation >> ON");
+
+	}
+	
+	/* Bad idea */
+	private static int count = 0;
+	private static int test = 0;
+	
+	@EventHandler
+	public void onPlayerAnimation(PlayerAnimationEvent e) {
+		
+		Player p = e.getPlayer();
+		
+		if (e.getAnimationType().equals(PlayerAnimationType.ARM_SWING)) {
+			
+			count++;
+			p.sendMessage("" + count);
+			
+			long time = 20;
+			new BukkitRunnable() {
+				
+				@Override
+				public void run() {
+					
+					test += 1; //>= 20 == AUTO 
+
+					if (test > time) {
+						
+						test = 0;
+						count = 0;
+						
+						p.sendMessage(ChatColor.RED + "ATTENTION");
+					}
+				}
+			}.runTaskLater(this, time); //Every seconds.
+		}
+		test = 0;
+		count = 0;
+	}
+}
