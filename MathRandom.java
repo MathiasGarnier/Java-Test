68179b38b7cf4fd58aae40b4f10bd1166ce27166
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-08-28 17:20:44 +0200

    Create MathRandom.java

---
+++ MathRandom.java
@@ -0,0 +1,17 @@
+package org.mathiasGarnier.Apprentissage;
+
+public class Main {
+
+    public static void main(String[] args) {
+		
+    	
+    	for (int i = 0; i < 100; i++) {
+    		
+    		double s = Math.random() * 10;
+    		
+    		s += i;
+    		
+    		System.out.println(s);
+    	}
+    }
+}
