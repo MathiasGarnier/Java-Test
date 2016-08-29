f9a7125c2de0b39d2d979e330094d71839efacf1
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-08-29 18:09:56 +0200

    Create GetSimpleName.java

---
+++ GetSimpleName.java
@@ -0,0 +1,13 @@
+package org.mathiasGarnier.Apprentissage;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+		
+	JObject o = new JObject(20);
+	JObject o1 = new JObject(20);
+	
+	System.out.println(o.getClass().getSimpleName());
+    }
+}
